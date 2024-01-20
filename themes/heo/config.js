808e67a67bd24652218ae6b55f39f85302848c48
w1501052468 <138978523+w1501052468@users.noreply.github.com>
2024-01-20 13:24:11 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,9 +9,9 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'king.freekj.link',
+  HEO_HERO_TITLE_1: '分享生活',
+  HEO_HERO_TITLE_2: '体验人生',
+  HEO_HERO_TITLE_3: 'king博客',
   HEO_HERO_TITLE_4: '初心不改',
   HEO_HERO_TITLE_5: 'king的个人博客',
   HEO_HERO_TITLE_LINK: 'https://king.freekj.link',
@@ -24,7 +24,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
