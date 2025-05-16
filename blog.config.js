dd6101aeaabbc6f7d8417d67921415fdbaf8534d
w1501052468 <138978523+w1501052468@users.noreply.github.com>
2025-05-16 12:35:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'king', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '听闻远方有你，我愿跋涉千里', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://king.freekj.me', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.862888.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'king, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
